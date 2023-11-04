28d96a7ea1b702a9f4045380ddffd239ccd30534
josuabad <josu.abad11@outlook.es>
2023-11-04 13:32:56 +0100

    Optimización fuct fuerza_bruta()

--- lib_e6.py
+++ lib_e6.py
@@ -12,7 +12,7 @@ def distancia(pto1: list[int], pto2: list[int]) -> float:
     return ((pto2[0] - pto1[0]) ** 2 + (pto2[1] - pto1[1]) ** 2) ** 0.5
 
 
-def fuerza_bruta(plano: list[int]) -> tuple:
+def fuerza_bruta(plano: list[int]) -> list:
     """
     Pares de puntos mas cercanos en un plano
 
@@ -20,11 +20,11 @@ def fuerza_bruta(plano: list[int]) -> tuple:
         plano (list): puntos
 
     Return:
-        tuple: puntos mas cercanos y distancia entre ellos
+        list: puntos mas cercanos y distancia entre ellos
     """
     punto1 = []
     punto2 = []
-    dist = 0
+    dist = float('inf')
     count = 0
 
     if len(plano) < 2:
@@ -35,65 +35,10 @@ def fuerza_bruta(plano: list[int]) -> tuple:
             if puntoB == plano[count]:
                 continue # No se compara a si mismo
             dist_temp = distancia(puntoA, puntoB)
-            if count == 0: # Primer cambio de las variables iniciadas
+            if dist_temp < dist:
                 dist = dist_temp
                 punto1 = puntoA
                 punto2 = puntoB
-            else:
-                if dist_temp < dist:
-                    dist = dist_temp
-                    punto1 = puntoA
-                    punto2 = puntoB
         count += 1 # Evita errores de redundancia
 
-    return punto1, punto2, dist
-
-
-def divide_conquer(plano: list[int]) -> tuple:
-    """
-    Pares de puntos mas cercanos en un plano
-
-    Args:
-        plano (list): puntos
-
-    Return:
-        tuple: puntos mas cercanos y distancia entre ellos
-    """
-    long_plano = len(plano)
-
-    if long_plano <= 1:
-        return float('inf')  # Si hay menos de 2 puntos, no se puede calcular distancia
-    
-    if long_plano == 2:
-        return distancia(plano[0], plano[1]) # Caso base
-    
-    # Ordena los puntos por coordenada x
-    puntos_ordenados_x = sorted(plano, key=lambda punto: punto[0]) # Timsort
-    
-    # Divide el conjunto en dos mitades
-    mitad = long_plano // 2
-    izquierda = puntos_ordenados_x[:mitad]
-    derecha = puntos_ordenados_x[mitad:]
-    
-    distancia_izquierda = divide_conquer(izquierda)
-    distancia_derecha = divide_conquer(derecha)
-    
-    # Encontrar la distancia mínima entre las dos mitades
-    distancia_minima_entre_mitades = min(distancia_izquierda, distancia_derecha)
-    
-    # Encontrar la franja con los puntos cercanos a la mitad de la division
-    pto_medio = puntos_ordenados_x[mitad][0]
-    franja = []
-    for punto in puntos_ordenados_x:
-        if abs(punto[0] - pto_medio) < distancia_minima_entre_mitades:
-            franja.append(punto)
-
-    # Compara las distancias por una posible menor
-    distancia_franja = float('inf')
-    for puntoA in range(len(franja)):
-        for puntoB in range(puntoA + 1, len(franja)):
-            distancia_actual = distancia(franja[puntoA], franja[puntoB])
-            distancia_franja = min(distancia_franja, distancia_actual)
-    
-    # Encontrar la distancia mínima entre las tres posibilidades
-    return min(distancia_minima_entre_mitades, distancia_franja)
+    return [punto1, punto2, dist]
