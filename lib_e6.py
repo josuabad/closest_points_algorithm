3d03160b0c1154088b98ee15844639f3f7b93cbe
josuabad <josu.abad11@outlook.es>
2023-11-04 11:07:15 +0100

    Optimizacion y solucion errores -> lib_e6 (fuerza_bruta())

--- lib_e6.py
+++ lib_e6.py
@@ -1,4 +1,4 @@
-def distancia(pto1: list, pto2: list):
+def distancia(pto1: list[int], pto2: list[int]) -> float:
     """
     Distacia entre 2 puntos -> formula euclidea
 
@@ -12,7 +12,7 @@ def distancia(pto1: list, pto2: list):
     return ((pto2[0] - pto1[0]) ** 2 + (pto2[1] - pto1[1]) ** 2) ** 0.5
 
 
-def fuerza_bruta(plano: list):
+def fuerza_bruta(plano: list[int]) -> tuple:
     """
     Pares de puntos mas cercanos en un plano
 
@@ -21,30 +21,29 @@ def fuerza_bruta(plano: list):
 
     Return:
         tuple: puntos mas cercanos y distancia entre ellos
-        boolean: input no es un plano (lista con puntos)
     """
     punto1 = []
     punto2 = []
     dist = 0
-    cont = 0
+    count = 0
 
     if len(plano) < 2:
-        return False
+        return TypeError # No permite que en el plano no haya mas de 1 punto
 
     for puntoA in plano:
         for puntoB in plano:
-            if puntoB == puntoA:
-                continue
+            if puntoB == plano[count]:
+                continue # No se compara a si mismo
             dist_temp = distancia(puntoA, puntoB)
-            if cont == 0:
+            if count == 0: # Primer cambio de las variables iniciadas
                 dist = dist_temp
                 punto1 = puntoA
                 punto2 = puntoB
-                cont += 1
             else:
                 if dist_temp < dist:
                     dist = dist_temp
                     punto1 = puntoA
                     punto2 = puntoB
+        count += 1 # Evita errores de redundancia
 
     return punto1, punto2, dist
