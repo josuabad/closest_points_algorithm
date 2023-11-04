ca12519eb3e65738409c59a6ce011d71b0410cf5
josuabad <josu.abad11@outlook.es>
2023-11-04 11:22:58 +0100

    divide_conquer() func V1

--- lib_e6.py
+++ lib_e6.py
@@ -47,3 +47,53 @@ def fuerza_bruta(plano: list[int]) -> tuple:
         count += 1 # Evita errores de redundancia
 
     return punto1, punto2, dist
+
+
+def divide_conquer(plano: list[int]) -> tuple:
+    """
+    Pares de puntos mas cercanos en un plano
+
+    Args:
+        plano (list): puntos
+
+    Return:
+        tuple: puntos mas cercanos y distancia entre ellos
+    """
+    long_plano = len(plano)
+
+    if long_plano <= 1:
+        return float('inf')  # Si hay menos de 2 puntos, no se puede calcular distancia
+    
+    if long_plano == 2:
+        return distancia(plano[0], plano[1]) # Caso base
+    
+    # Ordena los puntos por coordenada x
+    puntos_ordenados_x = sorted(plano, key=lambda punto: punto[0]) # Timsort
+    
+    # Divide el conjunto en dos mitades
+    mitad = long_plano // 2
+    izquierda = puntos_ordenados_x[:mitad]
+    derecha = puntos_ordenados_x[mitad:]
+    
+    distancia_izquierda = divide_conquer(izquierda)
+    distancia_derecha = divide_conquer(derecha)
+    
+    # Encontrar la distancia mínima entre las dos mitades
+    distancia_minima_entre_mitades = min(distancia_izquierda, distancia_derecha)
+    
+    # Encontrar la franja con los puntos cercanos a la mitad de la division
+    pto_medio = puntos_ordenados_x[mitad][0]
+    franja = []
+    for punto in puntos_ordenados_x:
+        if abs(punto[0] - pto_medio) < distancia_minima_entre_mitades:
+            franja.append(punto)
+
+    # Compara las distancias por una posible menor
+    distancia_franja = float('inf')
+    for puntoA in range(len(franja)):
+        for puntoB in range(puntoA + 1, len(franja)):
+            distancia_actual = distancia(franja[puntoA], franja[puntoB])
+            distancia_franja = min(distancia_franja, distancia_actual)
+    
+    # Encontrar la distancia mínima entre las tres posibilidades
+    return min(distancia_minima_entre_mitades, distancia_franja)
